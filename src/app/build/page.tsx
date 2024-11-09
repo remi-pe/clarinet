e50d40e1c93d2b0a946707a97e38fb8ae2798140
ioetbc <ioetbc@gmail.com>
2024-11-09 18:06:46 +0000

    use html react parse package instead of dangerouslySetHTML so that tailwind classes are correctly applied

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
 import {useStore} from "@/app/store";
+import parse from "html-react-parser";
 
 export default function BuildPage() {
   const {generatedCode} = useStore();
-
-  const extractClassNameFromJSX = (jsxString: string): string | null => {
-    const classNameMatch = jsxString.match(/className="([^"]+)"/);
-    return classNameMatch ? classNameMatch[1] : null;
-  };
 
   if (!generatedCode) return null;
 
   return (
     <>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <div
-          className={extractClassNameFromJSX(generatedCode) ?? undefined}
-          dangerouslySetInnerHTML={{
-            __html: generatedCode,
-          }}
-        />
+        <div>{parse(generatedCode)}</div>
       </div>
     </>
   );
